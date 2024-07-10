771e9a7387de980f92348463121fc263983262aa
GitUser6910 <idinahuiti890@gmail.com>
2024-07-10 19:21:37 +0200

    fourth update

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,11 +1,11 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
+// #![feature(custom_test_frameworks)]
+// #![test_runner(test_runner)]
+// #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use os::{QemuExitCode, exit_qemu, serial_println};
+use os::{exit_qemu, serial_println, QemuExitCode};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
